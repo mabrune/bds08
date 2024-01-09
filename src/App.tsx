1b6f30f6c389739b0dc4d80f79d21e094a0c3585
mabrune <mabrune.inf@gmail.com>
2024-01-09 01:25:48 -0300

    Navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './assets/styles/custom.scss';
+import Navbar from 'components/Navbar';
 
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello Github API</h1>
+    <>
+      <Navbar />
+      <h1>Hello Github API</h1>
+    </>
   );
 }
 
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+const Navbar = () => {
+    return (
+      <nav className="navbar navbar-expand-md bg-primary main-nav">    
+        <div className="container-fluid">
+          <a href="link" className="nav-logo-text"> 
+            <h4>Github API</h4> 
+          </a>
+        </div>
+      </nav>
+    );
+  };
+
+export default Navbar;
+
