72e531bd9d49b5096921e5d375d7614933f4dc08
mabrune <mabrune.inf@gmail.com>
2024-01-11 14:36:54 -0300

    ResultItem, Props

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -1,4 +1,6 @@
 import ProfileImg from 'assets/images/photo.png';
+import ResultItem from 'components/ResultItem';
+
 import './styles.css';
 
 const ResultCard = () => {
@@ -7,29 +9,13 @@ const ResultCard = () => {
          <div className="card-image">
             <img src={ProfileImg} alt="Foto do perfil" />
          </div>
-
          <div className="card-content">
             <h6>Informações</h6>
-            <div className="card-info">
-               <h3>Perfil:</h3>
-               <p className="line-break color-url">
-                  <b>https://api.github.com/users/acenelio</b>
-               </p>
-            </div>
-            <div>
-               <div className="card-info">
-                  <h3>Seguidores:</h3>
-                  <p className="line-break">4379</p>
-               </div>
-               <div className="card-info">
-                  <h3>Localidade:</h3>
-                  <p className="line-break">Uberlândia</p>
-               </div>
-               <div className="card-info">
-                  <h3>Nome:</h3>
-                  <p className="line-break">Nelio Alves</p>
-               </div>
-            </div>
+            <ResultItem title="Perfil:" description="https://api.github.com/users/acenelio"
+               classText="color-url" />
+            <ResultItem title="Seguidores:" description="4379" />
+            <ResultItem title="Localidade:" description="Uberlândia" />
+            <ResultItem title="Nome:" description="Nelio Alves" />
          </div>
       </div>
    );
